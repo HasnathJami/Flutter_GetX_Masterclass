b07c7feb28b73a3a26f8e508a21445815f9ee00b
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-08-30 01:26:38 +0600

    3. GetX: Dialog Alert, Bottom Sheet, Color Mode Change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,7 +16,79 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('GetX Masterclass'),
       ),
       body: Column(
-        children: [],
+        children: [
+          Card(
+            child: ListTile(
+              title: Text('GetX Dialog Alert'),
+              subtitle: Text('Getx dialog alert with getX'),
+              onTap: (){
+                Get.defaultDialog(
+                  title: "Delete chat",
+                  titlePadding: EdgeInsets.only(top: 20),
+                  // middleText: 'Are you sure you want to delete?',
+                    content: Column(
+                      children: [
+                        Text('Are you sure you want to delete?'),
+                        Text('Please let us know')
+                      ],
+                    ),
+                  contentPadding: EdgeInsets.all(20),
+                  // textConfirm: 'Yes',
+                  // textCancel: 'No',
+                  confirm: TextButton(
+                    onPressed: () {
+                    },
+                    child: Text('Ok'),
+                  ),
+                  cancel: TextButton(
+                    onPressed: (){
+                      // Navigator.pop(context);
+                       Get.back();
+                    },
+                    child: Text("Cancel"),
+                  ),
+                  barrierDismissible: false
+                );
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('GetX Bottom Sheet Dialog'),
+              subtitle: Text('Getx bottom sheet dialog with getX'),
+              onTap: (){
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light_mode),
+                          title: Text('Light Theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.light());
+                            Get.back();
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.dark_mode),
+                          title: Text('Dark Theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.dark());
+                            Get.back();
+                          },
+                        ),
+                      ],
+                    ),
+                  )
+                );
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         Get.snackbar(
